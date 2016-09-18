b7c9d5173cea79f399b297672f1cebec409fe02c
JJPavlik <jjpavlik@gmail.com>
2016-09-18 19:47:05 +0100

    Translated run.sh to English

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # glustergraph
-Show your gluster volumes and bricks in a graphicalish way
+
+Show your gluster volumes and bricks in a graphicalish way. This was tested on GlusterFS 3.4 and Java 7, back in 2014 so it may require some changes for newer GlusterFS versions. I'm not working with gluster anymore, that's why I have't tested this ever again.
+
+# Blogpost
+
+Unfortunately at the time I decided to write glustergraph my blog was entirely in Spanish, the particular entries were:
+
+http://viviendolared.blogspot.ie/2014/08/gluster-graficando-mi-gluster-i.html
+http://viviendolared.blogspot.ie/2014/08/gluster-graficando-mi-gluster-ii.html
+
+if you have any questions, you can just add a comment in english to the post and I'll be more than glad to answer.
+
+# Test instructions
+
+- Compile and generate the jar file by running
+
+./create_jar.sh
+
+- Then give run the run script
+
+./run.sh
+
+
--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 #
-#Pavlik salles Juan Jose 02/08/2014
+#Pavlik Salles Juan Jose 02/08/2014
 #
 #Version 0.1: 
-# - Admite volumenes Distribute y Distributed-Replicate sin importar el numero de ladrillos, ni de nodos, ni de replicas. Al menos en teoria.
-# - Debido a que los colores estan hardcoded el numero maximo de volumenes es 18 (TOCHANGE)
+# - Works with Distribute and Distributed-Replicate volumes, the number of bricks doesn't matter. At least in theory xD.
+# - Colours are kind of hardcoded for a number of 18 volumes (TOCHANGE)
 # - 
 #
-#Comentarios a jjpavlik@gmail.com o en https://viviendolared.blogspot.com
+#Comments to jjpavlik@gmail.com or https://viviendolared.blogspot.com :D
 #
-#Requiere:
+#Requieres:
 # apt-get install graphviz
 #
-#Funcionamiento:
-# 1-Armar el archivos hosts con los hosts que conforman el cluster de gluster, el archivo tiene el formato hostname:IP
-# 2-Armar el archivo de configuracion de gluster corriendo "gluster vol info > gluster_info"
-# 3-Ubicar ambos archivos en el mismo directorio donde se encuentra este script
-# 4-Lanzar este script ./lanzar
-# 5-La imagen sera out.png
-# --Los ladrillos de un mismo volumen tendran el mismo color
-# --Los ladrillos con replicas podran ser identificados por su codigo. Por ejemplo si el ladrillo 1 del nodo 2 es replica del ladrillo 2 del nodo 1, ambos tendran el mismo codigo
+#TIPS:
+# 1-Build gluster_info file by running:
+# gluster vol info > gluster_info
+# 2-Compile by running create_jar.sh
+# 3-Run script ./run.sh
+# 5-A PNG file called out.png should be generated
+# --Bricks that belong to the same volume will have the same colour.
+# --Replicated bricks can be identified by their REPX code.
 
 if [ ! -e gluster_info ]; then
  echo "Couldn't fine gluster_info file. You can build it running \"gluster vol info > gluster_info\"."
